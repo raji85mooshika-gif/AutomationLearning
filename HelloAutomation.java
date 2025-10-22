6882672f46dbe5d62168df6dc35d681d2c9f9357
Raji <raji85mooshika@gmail.com>
2025-10-22 18:16:08 +1000

    Added log message for login test branch

--- HelloAutomation.java
+++ HelloAutomation.java
@@ -3,5 +3,6 @@ public class HelloAutomation
 public static void main(String[] arg)
 {
     System.out.println("Hello World");
+    System.out.println("New test branch created - login tests coming soon!");
 }
 }
