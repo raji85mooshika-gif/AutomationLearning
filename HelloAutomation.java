455d8cec529fad5224924c13529ca39c67a28934
Raji <raji85mooshika@gmail.com>
2025-10-22 18:30:36 +1000

    More Practices added

--- HelloAutomation.java
+++ HelloAutomation.java
@@ -4,5 +4,7 @@ public static void main(String[] arg)
 {
     System.out.println("Hello World");
     System.out.println("New test branch created - login tests coming soon!");
+        System.out.println("MOre practice to commit the changes!");
+
 }
 }
